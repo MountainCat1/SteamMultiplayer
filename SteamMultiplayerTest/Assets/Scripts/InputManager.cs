a02283a3c615e412514d7896bd8faf887eb82a84
MountainCat <tomasz.banas123@gmail.com>
2022-09-29 02:24:50 +0200

    added InputManager.cs

---
+++ SteamMultiplayerTest/Assets/Scripts/InputManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public event Action InputRight;
+    public event Action InputLeft;
+    public event Action InputDown;
+    public event Action InputUp;
+    
+    private void Update()
+    {
+        var horizontalAxis = Input.GetAxis("Horizontal");
+        var verticalAxis = Input.GetAxis("Vertical");
+        
+        if(horizontalAxis > 0)
+            InputRight?.Invoke();
+        else if(horizontalAxis < 0)
+            InputLeft?.Invoke();
+        
+        if(verticalAxis > 0)
+            InputUp?.Invoke();
+        else if(verticalAxis < 0)
+            InputDown?.Invoke();
+    }
+}
--- SteamMultiplayerTest/Assets/Scripts/PlayerController.cs
+++ SteamMultiplayerTest/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,17 @@
-﻿using Network;
+﻿using System;
+using Network;
 using Unity.Netcode;
 using UnityEngine;
 
 [RequireComponent(typeof(ClientNetworkTransform))]
 public class PlayerController : NetworkBehaviour
 {
-    
+    private void Start()
+    {
+        if (!NetworkObject.IsOwner)
+        {
+            enabled = false;
+            return;
+        }
+    }
 }
